23db7eaaf81fc4c31bbedd7d1e917fbb10bc5748
= <=>
2018-12-23 12:21:00 +0300

    second commit

--- src/get/hup/GetHup.java
+++ src/get/hup/GetHup.java
@@ -15,7 +15,7 @@ public class GetHup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("welcom");
+        System.out.println("welcom3");
         // TODO code application logic here
     }
     
