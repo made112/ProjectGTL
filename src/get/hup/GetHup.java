ed82674820cf6a9368e86e67c3b17f4ac97b3e50
= <=>
2018-12-23 12:15:30 +0300

    first comment

---
+++ src/get/hup/GetHup.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package get.hup;
+
+/**
+ *
+ * @author HP 15-bs
+ */
+public class GetHup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("welcom");
+        // TODO code application logic here
+    }
+    
+}
